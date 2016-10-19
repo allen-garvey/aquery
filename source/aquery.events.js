990cd085fa7000d3bcf7563651d4cc7f5e7d9982
Allen Garvey <allengarvey@hotmail.com>
2016-10-19 10:57:25 -0400

    Update 'on' function to allow delegated event listeners

--- source/aquery.events.js
+++ source/aquery.events.js
@@ -1,10 +1,44 @@
 /*
 * Event methods
 */
-aQueryObject.prototype.on = function(eventName, callback){
+//Polyfill for Element.matches (required for delegated events)
+//source: https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+if (!Element.prototype.matches) {
+    Element.prototype.matches = 
+        Element.prototype.matchesSelector || 
+        Element.prototype.mozMatchesSelector ||
+        Element.prototype.msMatchesSelector || 
+        Element.prototype.oMatchesSelector || 
+        Element.prototype.webkitMatchesSelector ||
+        function(s) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                i = matches.length;
+            while (--i >= 0 && matches.item(i) !== this) {}
+            return i > -1;            
+        };
+}
+
+
+aQueryObject.prototype.on = function(eventName, selector, callback){
+	//check if using delegation
+	if(selector instanceof Function){
+		//not using delegation
+		var callbackFunc = selector;
+	}
+	else{
+		//using delegation
+		//based on: https://davidwalsh.name/event-delegate
+		var callbackFunc = function(e){
+			if(e.target && e.target.matches(selector)){
+    			callback(e);
+			}
+		};
+	}
+	//add event listers
 	this.each(function(i, element){
-		element.addEventListener(eventName, callback, false);
+		element.addEventListener(eventName, callbackFunc, false);
 	});
+
 };
 //calls onclick callback(s)
 aQueryObject.prototype.click = function(){
