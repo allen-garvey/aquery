c7336c0e7c016d8ea4f8505b2c45bd4b8d48ab12
Allen Garvey <allen.garvey@gmail.com>
2016-11-29 12:41:04 -0500

    Add remove function to dom manipulation methods and edit dom manipulation methods to automatically parse html if string is passed in

--- source/aquery.dom_manipulation.js
+++ source/aquery.dom_manipulation.js
@@ -3,22 +3,41 @@
 */
 
 //based on http://stackoverflow.com/questions/4793604/how-to-do-insert-after-in-javascript-without-using-a-library
-//content should already be element, such as that returned by aQuery.parseHTML()
+//content should already be element, such as that returned by aQuery.parseHTML() or html string
 aQueryObject.prototype.before = function(content){
+	if(typeof content == 'string'){
+		content = aQuery.parseHTML(content);
+	}
 	this.each(function(i, element){
 		element.parentNode.insertBefore(content, element);
 	});
 };
 
 aQueryObject.prototype.after = function(content){
+	if(typeof content == 'string'){
+		content = aQuery.parseHTML(content);
+	}
 	this.each(function(i, element){
 		element.parentNode.insertBefore(content, element.nextSibling);
 	});
 };
 
 aQueryObject.prototype.append = function(content){
+	if(typeof content == 'string'){
+		content = aQuery.parseHTML(content);
+	}
 	this.each(function(i, element){
 		element.appendChild(content);
+	});
+};
+
+//http://stackoverflow.com/questions/6104125/how-can-i-remove-an-element-in-javascript-without-jquery
+aQueryObject.prototype.remove = function(){
+	if(typeof content == 'string'){
+		content = aQuery.parseHTML(content);
+	}
+	this.each(function(i, element){
+		element.parentNode.removeChild(element);
 	});
 };
 
